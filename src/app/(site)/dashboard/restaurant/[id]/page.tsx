c64a53dca091294c8d4a08c922e13088166ccf92
Ashish2271 <ashispandey138c@gmail.com>
2024-09-09 01:19:44 +0530

    added by id fetch reservation

---
+++ src/app/(site)/dashboard/restaurant/[id]/page.tsx
@@ -0,0 +1,49 @@
+import { getServerSession } from "next-auth/next";
+import { redirect, notFound } from "next/navigation";
+import ReservationManagement from "@/components/ReservationManagement";
+import { authOptions } from "@/utils/auth";
+import { prisma } from "@/utils/prismaDB";
+
+async function getRestaurant(email: string) {
+  const restaurant = await prisma.restaurant.findFirst({
+    where: { owner: { email } },
+  });
+  return restaurant;
+}
+
+async function getSpecificFoodReservation(reservationId: string) {
+  const foodReservation = await prisma.foodReservation.findUnique({
+    where: {
+      id: reservationId,
+    },
+    include: {
+      user: true,
+      restaurant: true,
+    },
+  });
+  return foodReservation;
+}
+
+export default async function SpecificReservationPage({ params }: { params: { id: string } }) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/login");
+  }
+
+  const restaurant = await getRestaurant(session.user?.email as string);
+  if (!restaurant) {
+    redirect("/dashboard");
+  }
+
+  const reservation = await getSpecificFoodReservation(params.id);
+  if (!reservation || reservation.restaurantId !== restaurant.id) {
+    notFound();
+  }
+
+  return (
+    <div className="m-32">
+      <h1 className="mb-4 text-2xl font-bold">Food Reservation Details</h1>
+      <ReservationManagement reservations={[reservation]} restaurantId={restaurant.id} />
+    </div>
+  );
+}
